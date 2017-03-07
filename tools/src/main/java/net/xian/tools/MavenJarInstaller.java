2b6172723551fc08e712411c25562e3ebb5cbbb2
Dylan Deng <dxx1104@gmail.com>
2017-03-07 20:43:39 +0800

    delete 'ThinkingInJava' module, rewrite build scripts

--- tools/src/main/java/net/xian/tools/MavenJarInstaller.java
+++ tools/src/main/java/net/xian/tools/MavenJarInstaller.java
@@ -16,26 +16,30 @@ import java.util.Map;
  * @version 1.00 1/19/2017
  */
 public class MavenJarInstaller {
-    private static final String BASEDIR = "C:\\Users\\Dylan\\.gradle\\caches\\modules-2\\files-2.1";
+    private static final String BASEDIR = "/home/dylan/.gradle/caches/modules-2/files-2.1";
     private static final int BSIZE = 1024;
+
+    public static void main(String[] args) {
+        installMaven();
+    }
 
     public static void installMaven() {
         LinkedList<File> receiver = new LinkedList<>();
         FileUtil.scanDir(new File(BASEDIR), receiver);
         HashMap<File, File> newPathsMap = new HashMap<>(receiver.size());
         for (File file : receiver) {
-            String[] pathArray = file.getAbsolutePath().split("\\\\");
+            String[] pathArray = file.getAbsolutePath().split("/");
             ArrayList<String> newPathSagement = new ArrayList<>(4);
-            String newPackagePath = pathArray[7].replaceAll("\\.", "\\\\");
+            String newPackagePath = pathArray[7].replaceAll("\\.", "/");
             newPathSagement.add(newPackagePath);
             newPathSagement.add(pathArray[8]);
             newPathSagement.add(pathArray[9]);
             newPathSagement.add(pathArray[11]);
-            StringBuilder newPathBuilder = new StringBuilder("C:\\Users\\Dylan\\.m2\\repository");
-            newPathBuilder.append("\\\\");
+            StringBuilder newPathBuilder = new StringBuilder("/home/dylan/.m2/repository");
+            newPathBuilder.append("/");
             for (String sage : newPathSagement) {
                 newPathBuilder.append(sage);
-                newPathBuilder.append("\\\\");
+                newPathBuilder.append("/");
             }
             newPathsMap.put(file, new File(newPathBuilder.toString()));
         }
